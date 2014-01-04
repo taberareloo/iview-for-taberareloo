8b9ae5866e5c98ec6197fce1fee9544769f65318
YungSang <yungsang@gmail.com>
2014-01-03 18:09:54 -0800

    Bump the version to 1.11.0

--- iview.for.taberareloo.tbrl.js
+++ iview.for.taberareloo.tbrl.js
@@ -5,7 +5,7 @@
 // , "description" : "iview for Taberareloo"
 // , "include"     : ["background", "content"]
 // , "match"       : ["http://yungsang.github.io/iview-for-taberareloo/*"]
-// , "version"     : "1.10.1"
+// , "version"     : "1.11.0"
 // , "downloadURL" : "http://yungsang.github.io/iview-for-taberareloo/iview.for.taberareloo.tbrl.js"
 // }
 // ==/Taberareloo==
